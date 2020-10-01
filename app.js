52c47d5d121dd6124e9e68ccfb6dc3b8cb6053b2
Justin Rascoe <inbox@justinrascoe.com>
2020-10-01 17:31:39 -0400

    Changed name of main script file from server.js to app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+//TODO: Define a template engine
+//TODO: Create a template
+//TODO: Another todo example
+
+require('dotenv').config()
+const express = require('express')
+
+//Create Express App
+const app = express()
+
+//Define route for homepage
+app.get('/', (req, res)=> {
+    res.send('Hello World from Express 5')
+})
+
+//Start Express Server
+app.listen(process.env.PORT, () => console.log('Express 5 Server now listening on Port ' + process.env.PORT))
